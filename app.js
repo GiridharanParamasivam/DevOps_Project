c8c065ca672649b8fb1b99bf71f45ad1af71fa0d
GiridharanParamasivam <gparamas@my.centennialcollege.ca>
2024-12-03 22:02:25 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,8 @@
-console.log("DevOps Application Initialized");
+document.getElementById('loginForm').addEventListener('submit', function (e) {
+    e.preventDefault();
+    const username = document.getElementById('username').value;
+    const password = document.getElementById('password').value;
+
+    console.log(`Login attempted by ${username}`);
+    alert('Login successful!');
+});
