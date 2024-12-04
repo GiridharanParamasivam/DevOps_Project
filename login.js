0acc015385d094bb77522a71cc101f477987cf14
Giridharan Paramasivam <gparamas@my.centennialcollege.ca>
2024-12-03 21:33:35 -0500

    delete file

--- login.js
+++
@@ -1,6 +0,0 @@
-document.getElementById('login-form').addEventListener('submit', (event) => {
-    event.preventDefault();
-    const username = document.getElementById('username').value;
-    const password = document.getElementById('password').value;
-    console.log(`Username: ${username}, Password: ${password}`);
-});
