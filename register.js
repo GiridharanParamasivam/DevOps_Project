434a4a1b1c5179ebb2de1ac55016ee1fa3416936
GiridharanParamasivam <gparamas@my.centennialcollege.ca>
2024-12-03 21:32:58 -0500

    Delete register.js

--- register.js
+++
@@ -1,6 +0,0 @@
-document.getElementById('register-form').addEventListener('submit', (event) => {
-    event.preventDefault();
-    const newUsername = document.getElementById('new-username').value;
-    const newPassword = document.getElementById('new-password').value;
-    console.log(`New Username: ${newUsername}, New Password: ${newPassword}`);
-});
